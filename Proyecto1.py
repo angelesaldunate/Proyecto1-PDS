98862cbc5cda66563f5c22fe2b4a0aae0462be00
angelesaldunate <31292145+angelesaldunate@users.noreply.github.com>
2018-08-05 22:13:25 -0400

    Project 1 sort python

--- Proyecto1.py
+++ Proyecto1.py
@@ -1,4 +1,5 @@
 import time
+import operator
 
 startTime = time.time()
 CSV_DIRECTORY = "data/"
@@ -8,7 +9,7 @@ goals_hash = {}
 while (goal_line != ""):
     splitted_line = goal_line.strip().split(' ')
     required_file_name = splitted_line.pop(0)
-    required_position = splitted_line.pop(0)
+    required_position = int(splitted_line.pop(0))
     required_order = splitted_line.pop(0)
     ponderator_vector = [int(i) for i in splitted_line]
     touples = []
@@ -22,9 +23,17 @@ while (goal_line != ""):
                 ponderation += float(attr)
         touples.append((identifier, ponderation))
     goal_line = goals_file.readline()
-
     # COMPUTE THE RESULT
     result = ""
-    goals_hash[goal_line] = result
+    touples.sort(key=operator.itemgetter(1))
+    if required_order == 'ASC':
+        result = touples[required_position]
+    else:
+        result = touples[len(touples)-required_position]
 
+    goals_hash[goal_line] = result
+resultFile = open("resultsX.txt",'w' )
+for result in goals_hash.values():
+    resultFile.write(result[0]+"\n")
+resultFile.close()
 print ('The script took {0} second !'.format(time.time() - startTime))
