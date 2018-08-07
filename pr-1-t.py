27e191d5182c4564db60ca0ed44330aff0ce7350
e440 <jvejar1@miuandes.cl>
2018-08-06 22:44:01 -0400

    ..

--- pr-1-t.py
+++ pr-1-t.py
@@ -52,7 +52,8 @@ class Row:
 
     def get_ponderation(self,ponderator_vector):
         return numpy.dot(ponderator_vector,self.attrs)
-
+    def get_identifier(self):
+        return self.identifier
 class File:
     def __init__(self,file_name,rows_list):
         self.file_name=file_name
@@ -61,45 +62,99 @@ class File:
 
     def set_sorted_by_ponderator_vector(self,sorted_by_ponderator_vector):
         self.sorted_by_ponderator_vector=sorted_by_ponderator_vector
-startTime = time.time()
-CSV_DIRECTORY = "data/"
-goals_hash = {}
-ponderator_vectors=[]
-for i in range(3**10):
-    ponderator_vector=[int(pond) for pond in list("{0:b}".format(i).zfill(10))]
-    ponderator_vectors.append(ponderator_vector)
 
-total_time_seconds=10*60
-allFiles = glob.glob("data/*.csv")
-files_count=len(allFiles)
-max_seconds_for_file=total_time_seconds/files_count
-filesCases = {}
-for file in allFiles:  # para cada archivo de data
-    allCases = {}  # donde guardo los casos
-    file_start_time=time.time()
-    file_name=file.split("/")[-1]
-    csv_file = open(file, 'r')
-    first_line_len = len(csv_file.readline().strip().split(';')) - 1  # veo el largo de las columnas
-    file_rows=[]
+    def get_sorted_by_ponderator_vector(self):
+        return self.sorted_by_ponderator_vector
+
+    def set_key_in_sorted_by_ponderator_vector(self,key,value):
+        self.sorted_by_ponderator_vector[key]=value
+
+def get_rows_from_csv(csv_file_name):
+    csv_file = open(CSV_DIRECTORY+"/"+csv_file_name, 'r')
+    attrs_count = len(csv_file.readline().strip().split(';')) - 1  # veo el largo de las columnas
+    file_rows = []
     all_csv_file = list(csv_file.readlines())  # guardo el archivo en una lista
     for csv_line in all_csv_file:
         attrs = csv_line.strip().split(';')
         identifier = attrs.pop(0)
-        attrs=[float(attr) for attr in attrs]
-        row=Row(identifier,attrs)
+        attrs = [float(attr) for attr in attrs]
+        row = Row(identifier, attrs)
         file_rows.append(row)
+    csv_file.close()
+    return attrs_count,file_rows
 
-    file_object=File(file_name,file_rows)
-    sorted_by_ponderator={}
-    for ponderator_vector in ponderator_vectors:
+startTime = time.time()
+
+preprocess_mode=False
+CSV_DIRECTORY = "data/"
+goals_hash = {}
+
+total_time_seconds=10*60
+allFiles = glob.glob("data/*.csv")
+files_count=len(allFiles)
+
+file_objects_by_file_name={}
+goals_path="goals.txt"
+if preprocess_mode:
+    max_seconds_for_file=total_time_seconds/files_count
+    for file in allFiles:  # para cada archivo de data
+        allCases = {}  # donde guardo los casos
+        file_start_time=time.time()
+        file_name=file.split("/")[-1]
+        csv_file = open(file, 'r')
+        attrs_count,file_rows=get_rows_from_csv(file_name)
+        file_object=File(file_name,file_rows)
+
+        ponderator_vectors = []
+        for i in range(0,2**(attrs_count)):
+            ponderator_vector = [int(pond) for pond in list("{0:b}".format(i).zfill(10))]
+            ponderator_vectors.append(ponderator_vector)
+
+        sorted_by_ponderator={}
+        for ponderator_vector in ponderator_vectors:
+            sorted_rows=sorted(file_rows,key=lambda x:x.get_ponderation(ponderator_vector))
+            sorted_by_ponderator[tuple(ponderator_vector)]=sorted_rows
+            current_time=time.time()
+            delta_seconds=int(current_time-file_start_time)
+            if(delta_seconds>=max_seconds_for_file):
+                file_object.set_sorted_by_ponderator_vector(sorted_by_ponderator)
+                break
+        file_objects_by_file_name[file_name]=file_object
+    goals_path=raw_input("ingrese nombre del archivo goals")
+
+
+goals_file=open(goals_path,'r')
+goal_line = goals_file.readline()
+while goal_line != "":
+    splitted_line = goal_line.strip().split(' ')
+    required_file_name = splitted_line.pop(0)
+    required_position = int(splitted_line.pop(0))
+    required_order = splitted_line.pop(0)
+    ponderator_vector = [int(i) for i in splitted_line]
+    tuples = []
+
+    if (required_file_name not in file_objects_by_file_name):
+        attrs_count,rows=get_rows_from_csv(required_file_name)
+        file_objects_by_file_name[required_file_name]=File(required_file_name,rows)
+    file_object=file_objects_by_file_name[required_file_name]
+    if tuple(ponderator_vector) not in file_object.get_sorted_by_ponderator_vector:
         sorted_rows=sorted(file_rows,key=lambda x:x.get_ponderation(ponderator_vector))
-        sorted_by_ponderator[tuple(ponderator_vector)]=sorted_rows
-        current_time=time.time()
-        delta_seconds=int(current_time-file_start_time)
-        if(delta_seconds>=max_seconds_for_file):
-            file_object.set_sorted_by_ponderator_vector(sorted_by_ponderator)
-            break
-    filesCases[file.replace('data\\', '')] = allCases  # dejo como key el nombre del archivo
+        file_object.set_key_in_sorted_by_ponderator_vector(tuple(ponderator_vector),sorted_rows)
+
+    # COMPUTE THE RESULT
+    # bubbleSort(tuples)
+    # sorted(tuples, key=lambda x: x[1])
+    # heap_sort(tuples)
+    # print tuples
+    if(required_order=='DESC'):
+        required_position = len(file_object.get_sorted_by_ponderator_vector[tuple(ponderator_vector)]) - required_position
+
+    result = file_object.get_sorted_by_ponderator_vector[tuple(ponderator_vector)][required_position].get_identifier
+
+    goal_line = goals_file.readline()
+
+goals_file.close()
+
 # print (filesCases[list(filesCases.keys())[-1]])
 
 # PRE PROCESSING
